6347bc486e5df917ffcc4b9b6a3d33c120f84c03
Eldar Mirzabekov <prog.eldar@gmail.com>
2023-02-15 23:18:32 +0200

    feat(easy): resolve `Same Tree` problem

---
+++ solutions/easy/Same Tree.ts
@@ -0,0 +1,10 @@
+import { TreeNode } from "@types";
+
+export function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  // if both empty
+  if (!p && !q) return true;
+
+  if (!p || !q || p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
--- solutions/easy/index.ts
+++ solutions/easy/index.ts
@@ -3,3 +3,4 @@ export * from "./Add Binary";
 export * from "./Remove Duplicates from Sorted List";
 export * from "./Merge Sorted Array";
 export * from "./Binary Tree Inorder Traversal";
+export * from "./Same Tree";
