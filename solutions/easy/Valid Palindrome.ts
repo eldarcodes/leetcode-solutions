dbba68220de1e36c0f6779f4e2e22a7c2e9e0600
Eldar Mirzabekov <prog.eldar@gmail.com>
2023-02-26 00:08:57 +0200

    feat(easy): resolve `Valid Palindrome` problem

--- solutions/easy/Valid Palindrome.ts
+++ solutions/easy/Valid Palindrome.ts
@@ -2,4 +2,36 @@
 A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 Given a string s, return true if it is a palindrome, or false otherwise.
 */
-export function isPalindrome(s: string): boolean {}
+export function isPalindrome(s: string): boolean {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (!isAlphanumeric(s[left])) {
+      left++;
+      continue;
+    }
+
+    if (!isAlphanumeric(s[right])) {
+      right--;
+      continue;
+    }
+
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+function isAlphanumeric(char: string): boolean {
+  return (
+    (char >= "a" && char <= "z") ||
+    (char >= "A" && char <= "Z") ||
+    (char >= "0" && char <= "9")
+  );
+}
