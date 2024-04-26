ec5a9e1955919e699c2ff84e8264982ff00baaea
eldarcodes <prog.eldar@gmail.com>
2024-04-26 10:13:37 +0300

    feat(cli): update task template

--- cli/templates/task-template.ts
+++ cli/templates/task-template.ts
@@ -1,7 +1,14 @@
 import { camelCase } from "lodash";
 
 export const getTaskTemplate = (taskName: string) => {
-  return `export const ${camelCase(taskName)} = (input: any): any => {
+  return `/**
+  Input: 
+  Output: 
+
+  Time complexity = O()
+  Space complexity = O()
+*/
+export const ${camelCase(taskName)} = (input: any): any => {
   return null;
 };
 `;
